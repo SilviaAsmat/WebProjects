18d7b9aa7438fdafa1a9304d7d0f6031ec23ecf2
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-06-04 13:13:03 -0500

    Added restartQuiz()

--- Quiz Game/script.js
+++ Quiz Game/script.js
@@ -169,3 +169,9 @@ function showResults() {
     resultMessage.textContent = "Keep studying! You'll get better!";
   }
 }
+
+function restartQuiz() {
+  resultScreen.classList.remove("active");
+
+  startQuiz();
+}
