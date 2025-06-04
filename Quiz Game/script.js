95cd708243bf9252f02c4fef393088cac86b720d
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-06-04 13:11:22 -0500

    Created startQuiz()

--- Quiz Game/script.js
+++ Quiz Game/script.js
@@ -62,3 +62,26 @@ const quizQuestions = [
   },
 ];
 
+// QUIZ STATE VARS
+let currentQuestionIndex = 0;
+let score = 0;
+let answersDisabled = false;
+
+totalQuestionsSpan.textContent = quizQuestions.length;
+maxScoreSpan.textContent = quizQuestions.length;
+
+// event listeners
+startButton.addEventListener("click", startQuiz);
+restartButton.addEventListener("click", restartQuiz);
+
+function startQuiz() {
+  // reset vars
+  currentQuestionIndex = 0;
+  score = 0;
+  scoreSpan.textContent = 0;
+
+  startScreen.classList.remove("active");
+  quizScreen.classList.add("active");
+
+  showQuestion();
+}
